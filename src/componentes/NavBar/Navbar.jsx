4ec8e2dde2b741e745ed9984fa7d68441dbcc678
Francisco <franciscobravo643@gmail.com>
2024-09-10 12:58:52 -0300

    Mejorando del todo la sintaxis de codigo

---
+++ src/componentes/NavBar/Navbar.jsx
@@ -0,0 +1,24 @@
+import CartWidget from "../CartWidget/CartWidget"
+import "./NavBar.css"
+
+
+const Navbar = () => {
+
+  return (
+    <header>
+        <h1>Marolio Online</h1>
+
+    <nav>
+        <ul>
+            <li className="text-primary">Lacteos</li>
+            <li className="text-danger">Bebidas</li>
+            <li className="text-warning">Almacen</li>
+        </ul>
+    </nav>
+
+    <CartWidget/>
+    </header>
+  )
+}
+
+export default Navbar
